bb505b3316c86ec1e321c26ba0adddf73ff907c7
2023lewisrho <2023lewisrho@usbccollegiate.org>
2021-10-27 02:11:50 +0900

    finish JS, consolidate HTML into index.html, update index-css with center class, finish online order project, update gitignore

--- online_ordering_system/index.js
+++ online_ordering_system/index.js
@@ -20,7 +20,7 @@ async function get_menu(resturant) {
     try {
         const response = await fetch(url);
         const foods = await response.json();
-
+        console.log(foods);
         return foods;
     }
     catch(err) {
@@ -55,7 +55,7 @@ function resturant_to_element(resturant) {
     let rating = document.createElement("label");
 
     // set up resturant elem
-    element.classList = ["flex-item", "clearfix"];
+    element.classList = "flex-item";
     // set up icon image
     icon.setAttribute("src", resturant.ico);
     icon.setAttribute("alt", "Resturant Logo");
@@ -67,8 +67,130 @@ function resturant_to_element(resturant) {
     description.innerHTML = resturant.description;
     element.appendChild(description);
     //
-    rating.innerHTML = resturant.rating.rate + "/5.0";
+    console.log(resturant);
+    rating.innerHTML = "★" + resturant.rating.rating + "/5.0";
     element.appendChild(rating);
 
     return element;
+}
+
+function fooditem_to_element(fooditem) {
+    let element = document.createElement("div");
+    let icon = document.createElement("img");
+    let header = document.createElement("h4");
+    let description = document.createElement("p");
+    let price = document.createElement("label");
+    let rating = document.createElement("label");
+
+    element.classList = "flex-item";
+    // set up icon image
+    icon.setAttribute("src", fooditem.ico);
+    icon.setAttribute("alt", "Food Image");
+    element.appendChild(icon);
+    // set up header
+    header.innerHTML = fooditem.name;
+    element.appendChild(header);
+    // set up description
+    let desc = fooditem.food_type.toString();
+    desc.replace('[', '');
+    desc.replace(']', '');
+    description.innerHTML = desc;
+    element.appendChild(description);
+    //
+    price.innerHTML = "₩" + fooditem.price;
+    element.appendChild(price);
+
+    // nooooo dont use <br> to format!!!!!!
+    // haha <br> go 
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    element.appendChild(document.createElement("br"));
+
+    rating.innerHTML = "★" + fooditem.rating.rating + "/5.0";
+    element.appendChild(rating);
+
+    return element;
+}
+
+function set_pageheader(str) {
+    let header = document.getElementById("header");
+    header.innerHTML = str;
+}
+
+function remove_all_children(id) {
+    let elem = document.getElementById(id);
+    while (elem.lastChild) {
+        elem.removeChild(elem.lastChild);
+    }
+}
+
+// on load
+function main() {
+    let main_div = document.getElementById("root_view");
+    remove_all_children("root_view");
+    get_resturant().then((success) => {
+        success.forEach(element => {
+            let to_add = resturant_to_element(element);
+            to_add.onclick = prepareFoods;
+            set_pageheader("Resturants");
+            main_div.appendChild(to_add);
+        });
+    }, (failure) => {
+        console.error(failure);
+    });
+}
+
+main()
+
+function prepareFoods(event) {
+    var resturant_to_get = event.target || event.srcElement;
+    if (resturant_to_get.localName != "div") {
+        resturant_to_get = resturant_to_get.parentNode;
+    }
+
+    // get the h4 containing the resturant
+    var resturant = resturant_to_get.getElementsByTagName('h4')[0].innerHTML;
+    let main_div = document.getElementById("root_view");
+    remove_all_children("root_view");
+    get_menu(resturant).then((success) => {
+        success.forEach(element => {
+            let to_add = fooditem_to_element(element);
+            to_add.onclick = checkout;
+            set_pageheader("Menu of Resturant " + resturant);
+            main_div.appendChild(to_add);
+        });
+    }, (failure) => {
+        console.error(failure);
+    });
+}
+
+function checkout(event) {
+    remove_all_children("root_view");
+    set_pageheader("Checkout");
+
+    document.getElementById("form").hidden = false;
+    document.getElementById("submit").onclick = thank_you;
+}
+
+function thank_you(ev) {
+    remove_all_children("root_view");
+    let main_div = document.getElementById("root_view");
+    let thank = document.createElement("div");
+    let you = document.createElement("h1");
+
+    document.getElementById("form").hidden = true;
+    set_pageheader("Bought!");
+
+
+    thank.classList = "center";
+    you.innerHTML = "Thank you for your purchase!";
+
+    thank.appendChild(document.createElement("br"));
+    thank.appendChild(you);
+    main_div.appendChild(thank);
 }
--- online_ordering_system/server/src/main.rs
+++ online_ordering_system/server/src/main.rs
@@ -5,6 +5,21 @@ extern crate lazy_static;
 #[macro_use]
 extern crate serde;
 
+use std::collections::HashMap;
+use rocket::{
+    serde::json::Json,
+    http::{
+        Method,
+        Header
+    },
+    fairing::{Fairing, Info, Kind},
+    Request,
+    Response
+};
+use serde::{Serialize, Deserialize};
+use async_trait::async_trait;
+
+
 // WOOOOOOOOOOOOOOOOH BECOME A RUSTACEAN WOOOOOOOOOOOOOOH
 // (you're being rustacean hypno right now)
 // R RR RR                  
@@ -127,7 +142,7 @@ lazy_static! {
 
         map.insert(
             Resturant::new(
-                "Mr. Bolat Kebab & Pizza",
+                "Mr. Bolat Kebab &amp; Pizza",
                 "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.sosfactory.com%2Fwp-content%2Fuploads%2F2016%2F12%2Ffull-restaurant-logo-min.png&f=1&nofb=1",
                 "The Best Pizza and late-night Kebabs in town",
                 4.7_f32
@@ -293,10 +308,6 @@ lazy_static! {
     };
 }
 
-use std::collections::HashMap;
-use rocket::serde::json::Json;
-use serde::{Serialize, Deserialize};
-
 // we have to do this because floats are not Eq, Ord, and Hash
 // In an actual application this would be a proper rational/f32 wrapper
 // but you see, that requires actual effort than an From impl
@@ -386,8 +397,36 @@ fn foods(resturant: String) -> Json<Vec<FoodItem>> {
     }
 }
 
+pub struct Cors;
+
+#[async_trait]
+impl Fairing for Cors {
+    fn info(&self) -> Info {
+        Info {
+            name: "Cross-Origin-Resource-Sharing Middleware",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self,
+                             request: &'r Request<'_>,
+                             response: &mut Response<'r>) {
+        response.set_header(Header::new(
+            "access-control-allow-origin",
+            "*",
+        ));
+        response.set_header(Header::new(
+            "access-control-allow-methods",
+            "GET, PATCH, OPTIONS",
+        ));
+    }
+}
+
+
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![resturant, foods])
+    rocket::build()
+        .attach(Cors)
+        .mount("/", routes![resturant, foods])
 }
